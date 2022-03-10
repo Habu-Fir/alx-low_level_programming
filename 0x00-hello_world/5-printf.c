459039470cf62fbaaa949cb5344a659226370e90
Habu-Fir <tohabtish16@gmail.com>
2022-03-10 03:10:27 -0800

    Display text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/*
+ * this program display with proper grammar, 
+ * but the outcome is a piece of art, using printf*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
