d5e2f76fd04c6b1e02518f2e3938e7b3056350ea
Habu-Fir <tohabtish16@gmail.com>
2022-03-10 03:14:38 -0800

    Display text using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
  * this program display with proper grammar, 
- * but the outcome is a piece of art, using printf*/
+ * but the outcome is a piece of art, using printf
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
