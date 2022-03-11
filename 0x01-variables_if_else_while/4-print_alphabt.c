b4b5b7dea57e95ddf8263074c2df58faf36cf2ee
Habu-Fir <tohabtish16@gmail.com>
2022-03-11 06:25:21 -0800

    Task 4 added to 0x01c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -  print alphabt in lower case,
+ * followed by new line, except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
