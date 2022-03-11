e2e70bedd133395bbb1f95694b888642de3f1ad7
Habu-Fir <tohabtish16@gmail.com>
2022-03-11 06:41:59 -0800

    Task 5 added to 0x01c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit number of base 10 starting from 0,
+ * followed by new line
+ * Return : always 0 success
+ */
+int main (void)
+{
+	int i;
+
+	for (int i = 0; i < 9; i++)
+	{
+		printf(i);
+	}
+	printf("%d\n");
+	return (0);
+
+}
+
