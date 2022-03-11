db773cad8c40808b5cc244ee058f6befdfb494bf
Habu-Fir <tohabtish16@gmail.com>
2022-03-11 06:48:35 -0800

    Task 5 added to 0x01c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * main - print all single digit number of base 10 starting from 0,
  * followed by new line
- * Return : always 0 success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
